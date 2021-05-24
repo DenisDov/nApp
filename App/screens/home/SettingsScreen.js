9468643423d68b810db7faecf69f531ebd568d90
DenisDov <dovzdenis@gmail.com>
2021-05-24 13:58:56 +0300

    safearea

--- App/screens/home/SettingsScreen.js
+++ App/screens/home/SettingsScreen.js
@@ -1,12 +1,75 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Box, Text } from '../../theme';
 
 const SettingsScreen = () => {
 	return (
-		<View>
-			<Text>Settings here</Text>
-		</View>
+		<SafeAreaView style={styles.container} edges={['right', 'bottom', 'left']}>
+			<ScrollView>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+				<Box padding="m">
+					<Text>Settings here</Text>
+				</Box>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+});
 
 export default SettingsScreen;
